b99b9dfad5817a1a3fd26ed7a152c3fb8fc8b60d
Nickolas Heckman <nickolas.heckman@gmail.com>
2022-03-17 17:10:41 -0700

    Initial project configuration

---
+++ src/test/java/xyz/heckman/demo/rsocket/RsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.heckman.demo.rsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
