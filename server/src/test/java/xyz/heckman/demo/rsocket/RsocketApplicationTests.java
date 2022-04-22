bef4dd1150df9cdafab944114618d2075d39ad7e
Nickolas Heckman <nickolas@afero.io>
2022-04-22 09:02:16 -0700

    Assigns random port to RSocket server for test execution

--- server/src/test/java/xyz/heckman/demo/rsocket/RsocketApplicationTests.java
+++ server/src/test/java/xyz/heckman/demo/rsocket/RsocketApplicationTests.java
@@ -1,13 +1,20 @@
 package xyz.heckman.demo.rsocket;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RsocketApplicationTests {
+
+	@Value("${local.rsocket.server.port}")
+	private int rSocketPort;
 
 	@Test
 	void contextLoads() {
+		System.out.println(String.format("RSocket server running on port: %d", rSocketPort));
 	}
 
 }
